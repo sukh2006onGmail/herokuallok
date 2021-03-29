bec53d7e81177443a0f2eca77699ff42bdd0cdc2
sk <sukh2006on@gmail.com>
2021-03-29 09:38:07 +0800

    ad

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -99,15 +99,15 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+# LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+# TIME_ZONE = "UTC"
 
-USE_I18N = True
+# USE_I18N = True
 
-USE_L10N = True
+# USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
